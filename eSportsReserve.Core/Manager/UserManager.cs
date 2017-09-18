2ec4523000e7e6ffcb5a323a928d7d0e1fa51359
lawrencegrey <lawrencegrey.0125@gmail.com>
2017-09-17 21:37:31 -0500

    latest changes

--- eSportsReserve.Core/Manager/UserManager.cs
+++ eSportsReserve.Core/Manager/UserManager.cs
@@ -31,16 +31,69 @@ namespace eSportsReserve.Core.Manager
         private IQueryable<Avatar> AvatarInfo()
         {
             return _avatar
-                .OrderBy(f => f.Name);
+              .OrderBy(a => a.UserId);
         }
 
-        private IQueryable<Reservation> ReservationInfo()
+        private IQueryable<User> UserInfo()
         {
-            return _user
-                .OrderBy(r => r.GroupId);
+            return _user;
+               
         }
 
-       
+        //avatar
+        public async Task<IList<Avatar>> GetUserAvatar(Guid userid)
+        {
+            return await AvatarInfo()
+                .Where(a => a.UserId.Equals(userid))
+                .ToListAsync();
+        }
+
+        public Task CreateAvatar(Avatar avatar)
+        {
+            _avatar.Add(avatar);
+            return _dbContext.SaveChangesAsync();
+        }
+
+        public Task UpdateAvatar(Avatar avatar)
+        {
+            _avatar.Update(avatar);
+            return _dbContext.SaveChangesAsync();
+        }
+
+        public Task DeleteAvatar(Avatar avatar)
+        {
+            _avatar.Remove(avatar);
+            return _dbContext.SaveChangesAsync();
+        }
+
+        //user
+
+        public async Task<IList<User>> GetUserDetails(Guid userid)
+        {
+            return await UserInfo()
+                .Where(a => a.Id.Equals(userid))
+                .ToListAsync();
+        }
+
+
+        public Task CreateUser(User user)
+        {
+            _user.Add(user);
+            return _dbContext.SaveChangesAsync();
+        }
+
+        public Task UpdateUser(User user)
+        {
+            _user.Update(user);
+            return _dbContext.SaveChangesAsync();
+        }
+
+        public Task DeleteUser(User user)
+        {
+            _user.Remove(user);
+            return _dbContext.SaveChangesAsync();
+        }
+
 
     }
 }
